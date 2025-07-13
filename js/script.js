aa2242cf4423a8e82863e2b17f03c139d78e5f21
Oleksandr Nokiroma <sashamorykon@gmail.com>
2025-07-13 18:48:10 +0300

    Add tabs and animation

---
+++ js/script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () =>{
+    const tabs = document.querySelectorAll(".tabheader__item"); 
+    const tabContent = document.querySelectorAll(".tabcontent");
+    const tabsParent = document.querySelector(".tabheader__items");
+
+    function hideTubContent(){
+        tabContent.forEach(item =>{
+            item.classList.add('hide')
+            item.classList.remove('show','fade')
+        } )
+
+        tabs.forEach(item =>{
+            item.classList.remove('tabheader__item_active');
+        })
+    }
+
+    function showTabContent(i = 0){
+        tabContent[i].classList.add('show', 'fade')
+        tabContent[i].classList.remove('hide')
+        tabs[i].classList.add('tabheader__item_active')
+    }
+
+    tabsParent.addEventListener('click', (event) =>{
+        const target = event.target;
+
+        if(target && target.classList.contains('tabheader__item')){
+            tabs.forEach((item, i) =>{
+                if(target == item){
+                    hideTubContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    })
+
+   
+
+})
