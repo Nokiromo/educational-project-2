6acaff4fa2427ef895ea1ff61cec8b85d96eece3
Oleksandr Nokiroma <sashamorykon@gmail.com>
2025-07-31 21:20:05 +0300

    add regular axpressions

--- js/script.js
+++ js/script.js
@@ -326,7 +326,7 @@ function showThanksModel(massage){
 }
 
 //slider
-
+const slider = document.querySelector('.offer__slider')
 const current =  document.querySelector('#current')
 const total =  document.querySelector('#total')
 const prev = document.querySelector('.offer__slider-prev')
@@ -357,15 +357,59 @@ slides.forEach(e =>{
     e.style.width = width;
 })
 
+slider.style.position = 'relative'
 
+const indicators = document.createElement('ol')
+const dots = [];
 
+indicators.classList.add('carousel-indicators')
+indicators.style.cssText = `
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;`
+slider.append(indicators)
+
+for(let i = 0; i < slides.length; i++){
+    const dot =  document.createElement('li')
+    dot.setAttribute('data-slide-to', i + 1)
+    dot.style.cssText = `
+        box-sizing: content-box;
+        flex: 0 1 auto;
+        width: 30px;
+        height: 6px;
+        margin-right: 3px;
+        margin-left: 3px;
+        cursor: pointer;
+        background-color: #fff;
+        background-clip: padding-box;
+        border-top: 10px solid transparent;
+        border-bottom: 10px solid transparent;
+        opacity: .5;
+        transition: opacity .6s ease;`
+        if(i == 0){
+            dot.style.opacity = 1
+        }
+    indicators.append(dot)
+    dots.push(dot)
+}
+
+function reg (str){
+ return +str.replace(/\D/g, '')
+}
 
 next.addEventListener('click', () =>{
-    if(ofset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+    if(ofset == reg(width) * (slides.length - 1)){
         ofset = 0
     }
     else{
-        ofset += +width.slice(0, width.length - 2)
+        ofset += reg(width)
     }
     slidsField.style.transform = `translateX(-${ofset}px)`
 
@@ -380,14 +424,17 @@ next.addEventListener('click', () =>{
     }else{
         current.textContent = index
     }
+
+    dots.forEach(dot => dot.style.opacity = '.5')
+    dots[index - 1].style.opacity = 1;
 })
 
 prev.addEventListener('click', () =>{
     if(ofset == 0){
-        ofset = +width.slice(0, width.length - 2) * (slides.length - 1)
+        ofset = reg(width) * (slides.length - 1)
     }
     else{
-        ofset -= +width.slice(0, width.length - 2)
+        ofset -= reg(width)
     }
     slidsField.style.transform = `translateX(-${ofset}px)`
 
@@ -402,6 +449,30 @@ prev.addEventListener('click', () =>{
     }else{
         current.textContent = index
     }
+
+    dots.forEach(dot => dot.style.opacity = '.5')
+    dots[index - 1].style.opacity = 1;
+})
+
+dots.forEach(dot =>{
+    dot.addEventListener('click', (e)=>{
+        const slideTo = e.target.getAttribute('data-slide-to')
+
+        index = slideTo;
+        ofset = reg(width) * (slideTo - 1)
+        
+        slidsField.style.transform = `translateX(-${ofset}px)`
+        
+        if(slides.length < 10){
+        current.textContent = `0${index}`
+        }else{  
+        current.textContent = index
+        }
+        
+        dots.forEach(dot => dot.style.opacity = '.5')
+        dots[index - 1].style.opacity = 1;
+        
+    })
 })
 
 
